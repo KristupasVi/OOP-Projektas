e0da3476a72ece6cb68bf91e0d591ff1b1235343
Kristupas <kristupas.tadas@gmail.com>
2025-06-05 18:01:27 +0300

    Veikia Ideja dar be Threadu

--- Masteris/Masteris/Master.cs
+++ Masteris/Masteris/Master.cs
@@ -12,14 +12,19 @@
         {
 
             string pipe1Pav = "PipeA";
-            //string pipe2Pav = "PipeB";
             string folderio1Path = @"C:\Users\krist\OneDrive\Desktop\Uzrasai";
-            //string folderio2Path = @"C:\Users\krist\OneDrive\Desktop\Pasakos";
+            string rezultatas1 = "DuomenysA";
+            string pipe2Pav = "PipeB";
+            string folderio2Path = @"C:\Users\krist\OneDrive\Desktop\Pasakos";
+            string rezultatas2 = "DuomenysB";
 
             SiuntimasScaneriui(pipe1Pav, folderio1Path);
-          //  SiuntimasScaneriui(pipe2Pav, folderio2Path);
+            GautiRezultatai(rezultatas1);
+            SiuntimasScaneriui(pipe2Pav, folderio2Path);
+            GautiRezultatai(rezultatas2);
+
         }
-      
+
         static void SiuntimasScaneriui(string pipePav, string folderioPath)
         {
             using var serveris = new NamedPipeServerStream(pipePav, PipeDirection.Out); //  NamedPipeClientStream tas kuris jungiasi prie serverio
@@ -32,5 +37,22 @@
 
             Console.WriteLine("Master: Išsiuntė folderio path");
         }
+        static void GautiRezultatai(string pipePav)
+        {
+            using var infoGavimas = new NamedPipeServerStream(pipePav, PipeDirection.In);
+            Console.WriteLine("Master: Laukia rezultatų iš scanerio...");
+            infoGavimas.WaitForConnection();
+            Console.WriteLine("Master: Prisijunge Scanris");
+
+            using var skaitytuvas = new StreamReader(infoGavimas);
+            string? line;
+            while ((line = skaitytuvas.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine($"Rezultatai gauti iš {pipePav}");
+        }
     }
+
+
 }
---
+++ ScanerisB/Scaneris/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO.Pipes;
+class ScanerisB
+{
+    static void Main(string[] args)
+    {
+        string pipePav = "PipeB";
+        string pipeAtgal = "DuomenysB";
+
+        string gautasPath = GautiFolderioPath(pipePav);
+
+        if (!string.IsNullOrEmpty(gautasPath))
+        {
+            Console.WriteLine($"Gautas folderio path iš master: {gautasPath}");
+            NuskaitytiFailus(gautasPath, pipeAtgal);
+        }
+        else
+        { 
+            Console.WriteLine("Nepavyko gauti Folderio path iš master");
+        }
+    }
+
+    static string GautiFolderioPath(string pipePav)
+    {
+        using var Scaneris  = new NamedPipeClientStream(".", pipePav, PipeDirection.In); // NamedPipeServerStream tas kuris laukia kol prisijungs tas iš Mastrer su NamedPipeClientStream
+        Console.WriteLine("Scaneris: Jungiasi prie master");
+        Scaneris.Connect();
+        Console.WriteLine("Scaneris: Prisijungta");
+
+        using var skaitytuvas = new StreamReader(Scaneris);
+        return skaitytuvas.ReadLine();
+    }
+
+    static void NuskaitytiFailus(string path, string pipeAtgal)
+    {
+        if (!Directory.Exists(path))
+        {
+            Console.WriteLine($"Katalogas neegzistuoja: {path}");
+            return;
+        }
+
+        string[] failai = Directory.GetFiles(path, "*.txt");
+
+        Console.WriteLine($"Rasta tiek {failai.Length}, .txt failų:");
+
+       
+        using var siuntimasAtgal = IssiuntimasImaster(pipeAtgal);
+        using var scaneris = new StreamWriter(siuntimasAtgal) { AutoFlush = true };
+
+        foreach (string failas in failai)
+        {
+            string tekstas = File.ReadAllText(failas);
+            var zodziuStatistika = SkaiciuotiZodzius(tekstas);
+
+            scaneris.WriteLine($"\nKnygos {Path.GetFileName(failas)} statistika: ");
+            foreach (var pora  in zodziuStatistika)
+            {
+                scaneris.WriteLine($"{pora.Key}:{pora.Value}");
+            }
+        }
+
+        scaneris.WriteLine("\nVISOS KNYGOS BUVO NUSKENUOTOS");
+    }
+
+    static NamedPipeClientStream IssiuntimasImaster(string pipePav)
+    {
+        var siuntimasMasteriui = new NamedPipeClientStream(".", pipePav, PipeDirection.Out);
+        Console.WriteLine("Scaneris: Jungiamasi prie Master rezultato pipe:");
+        siuntimasMasteriui.Connect();
+        Console.WriteLine($"Prisijungta prie {pipePav}");
+        return siuntimasMasteriui;
+    }
+    static Dictionary<string, int> SkaiciuotiZodzius(string tekstas)
+    {
+        var rezultatas = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+        var zodziai = tekstas.Split(new[] { ' ', '\r', '\n', '\t', '.', ',', ';', ':', '!', '?', '(', ')', '[', ']', '{', '}', '"', '\'' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var zodis in zodziai)
+        {
+            if(rezultatas.ContainsKey(zodis))
+                rezultatas[zodis]++;
+            else
+                rezultatas[zodis] = 1;
+        }
+        return rezultatas;
+    }
+}
